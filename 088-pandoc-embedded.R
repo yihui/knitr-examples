240b47afcfffbdac97d40d7b3767451979017e2f
Yihui Xie <xie@yihui.name>
2013-07-01 15:25:41 -0700

    an example for yihui/knitr#547

--- 088-pandoc-embedded.R
+++ 088-pandoc-embedded.R
@@ -11,3 +11,5 @@ pandoc('088-pandoc-embedded.md')
 # it is also possible to convert md to pdf through xelatex
 pandoc('088-pandoc-embedded.md', format = 'latex')
 
+# multiple output formats
+pandoc('088-pandoc-embedded.md', format = c('latex', 'html', 'odt'))
