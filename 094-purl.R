a16cd7eb4db715478033e9c37468e46cf603b370
Yihui Xie <xie@yihui.name>
2014-12-29 16:00:03 -0600

    changes due to yihui/knitr@f8c972683a8bfa6bf1f90006b14a3497132d89b6

--- 094-purl.R
+++ 094-purl.R
@@ -1,19 +1,13 @@
-
 ## ----test-a, eval=TRUE---------------------------------------------------
 1+1
 strsplit('hello world', ' ')
 
-
 ## ----test-b, fig.width=10, fig.height=7----------------------------------
 if (FALSE) plot(1:10)
-
 
 ## ----test-c, eval=FALSE--------------------------------------------------
 ## paste(letters, collapse = '|')
 ## if (1 == 1) {
 ##   'Awesome!'
 ## }
-
-
-
 
