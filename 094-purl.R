03a7984985d5bbb1a49810620b3193d3994960da
Yihui Xie <xie@yihui.name>
2019-03-08 13:04:34 -0600

    changes caused by https://github.com/yihui/knitr/commit/46826deffbe30815eab2c159dcc17966fca90967 and https://github.com/yihui/knitr/commit/f2a636a4

--- 094-purl.R
+++ 094-purl.R
@@ -2,8 +2,10 @@
 1+1
 strsplit('hello world', ' ')
 
+
 ## ----test-b, fig.width=10, fig.height=7----------------------------------
 if (FALSE) plot(1:10)
+
 
 ## ----test-c, eval=FALSE--------------------------------------------------
 ## paste(letters, collapse = '|')
