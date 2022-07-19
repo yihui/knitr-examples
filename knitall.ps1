f2cc2eb8ab8cdb3cd91364d7e9d8faeb4a6aae9a
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-07-19 16:47:06 +0200

    Add a knitall equivalent for powershell

---
+++ knitall.ps1
@@ -0,0 +1,12 @@
+
+Get-ChildItem -Path "[0-9]*" |
+  Where-Object {$_.Extension -match "\.(brew|R(nw|md|tex|html|rst|asciidoc|textile))"} |
+  Where-Object {$_.Basename -nomatch "^051-"} |
+  ForEach-Object { ./k.ps1 $_ }
+
+Rscript -e "knitr::purl('094-purl.Rmd')"
+mv 078-chinese-big5.tex 078-chinese-big5.ctx
+# Explicitly run caching test twice
+./k.ps1 "101-cache-levels.Rmd"
+# load_cache() requires running twice
+./k.ps1 "114-load-cache.Rmd"
