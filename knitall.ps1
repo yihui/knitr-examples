41a65b2384e55355138428d07f60cb2c4875d264
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-07-25 07:06:02 -0400

    typo

--- knitall.ps1
+++ knitall.ps1
@@ -1,7 +1,7 @@
 
 Get-ChildItem -Path "[0-9]*" |
   Where-Object {$_.Extension -match "\.(brew|R(nw|md|tex|html|rst|asciidoc|textile))"} |
-  Where-Object {$_.Basename -nomatch "^051-"} |
+  Where-Object {$_.Basename -notmatch "^051-"} |
   ForEach-Object { ./k.ps1 $_ }
 
 Rscript -e "knitr::purl('094-purl.Rmd')"
