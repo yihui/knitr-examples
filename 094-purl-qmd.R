46c8d1db0cf0c9ab04432444079927324c4c3688
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-01-06 20:16:28 +0100

    Add some tests for Qmd (#86)

---
+++ 094-purl-qmd.R
@@ -0,0 +1,19 @@
+#| label: test-a
+#| eval: true
+1+1
+strsplit('hello world', ' ')
+
+
+#| label: test-b
+#| fig.width: 10.0
+#| fig.height: 7.0
+if (FALSE) plot(1:10)
+
+
+#| label: test-c
+#| eval: false
+## paste(letters, collapse = '|')
+## if (1 == 1) {
+##   'Awesome!'
+## }
+
--- knitall.ps1
+++ knitall.ps1
@@ -1,10 +1,12 @@
 
 Get-ChildItem -Path "[0-9]*" |
   Where-Object {$_.Extension -match "\.(brew|R(nw|md|tex|html|rst|asciidoc|textile))"} |
-  Where-Object {$_.Basename -nomatch "^051-"} |
+  Where-Object {$_.Basename -notmatch "^051-"} |
   ForEach-Object { ./k.ps1 $_ }
 
-Rscript -e "knitr::purl('094-purl.Rmd')"
+# special qmd tests
+Rscript -e "knitr::convert_chunk_header('094-purl.Rmd', '094-purl.qmd', 'yaml');"
+Rscript -e "knitr::knit('094-purl.qmd', output = '094-purl-qmd.md'); knitr::purl('094-purl.qmd', output = '094-purl-qmd.R')"
 mv 078-chinese-big5.tex 078-chinese-big5.ctx
 # Explicitly run caching test twice
 ./k.ps1 "101-cache-levels.Rmd"
