ee286ddaa64dd5eac12ed13c8b9d4ff0cbc7fe68
Yihui Xie <xie@yihui.name>
2023-10-20 14:02:14 -0500

    update purl results

--- 094-purl.R
+++ 094-purl.R
@@ -3,7 +3,7 @@
 strsplit('hello world', ' ')
 
 
-## ----test-b, fig.width=10, fig.height=7----------------------------------
+## ----test-b, fig.width=10, fig.height=7, dev='svg', dpi = 120------------
 if (FALSE) plot(1:10)
 
 
